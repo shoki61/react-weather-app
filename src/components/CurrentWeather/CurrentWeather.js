28b68915c14b3a6bc5c8c18d1114c306f2d205fe
Sohrat <sohrat6128@gmail.com>
2020-11-24 22:09:56 +0300

    style current weather card

--- src/components/CurrentWeather/CurrentWeather.js
+++ src/components/CurrentWeather/CurrentWeather.js
@@ -3,23 +3,23 @@ import React, { Component } from 'react';
 
 import './CurrentWeather.css';
 
-import img from '../../assets/icons/02d.png';
+import img from '../../assets/icons/11n.png';
 
 class CurrentWeather extends Component{
     render(){
         return(
             <div className='Current-Weather-Card'>
-                <p>Trabzon / TR</p>
+                <p className='City-Name'>Trabzon / TR</p>
                 <div className='Weather-Status'>
                     <img src={img} alt='img'/>
-                    <p>25C</p>
+                    <p className='Temp'>25°C</p>
                 </div>
                 <div className='Weather-Details'>
-                    <div>
+                    <div className='Details'>
                         <p>Pressure: </p>
                         <p>Humidity:</p>
                     </div>
-                    <div>
+                    <div className='Details'>
                         <p>Wind:</p>
                         <p>Clouds:</p>
                     </div>
