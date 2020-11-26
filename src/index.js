5b67ebd01fd6f7177bf1b3755edf6b5a5c52c945
Sohrat <sohrat6128@gmail.com>
2020-11-26 20:59:06 +0300

    connect the app to the redux store

--- src/index.js
+++ src/index.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+
+import reducer from './store/reducers/weather';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const store = createStore(reducer, applyMiddleware(thunk));
+
 ReactDOM.render(
         <React.StrictMode>
             <BrowserRouter>
-                <App />
+                <Provider store={store}>
+                    <App />
+                </Provider>
             </BrowserRouter>
         </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
