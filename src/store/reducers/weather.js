8e3aa29f7e756db84e8ef1c2f15e8dd6e165f5ff
Sohrat <sohrat6128@gmail.com>
2020-11-26 20:00:56 +0300

    set initial state elements

--- src/store/reducers/weather.js
+++ src/store/reducers/weather.js
@@ -2,13 +2,16 @@ import * as actionTypes from '../actions/actionTypes';
 
 const initialState = {
     currentWeather: null,
+    hourlyForecast: null,
+    weeklyForecast: null,
+    favorites:null,
     error: null,
     loading: false
 }
 
 const reducer = (state = initialState, action) => {
     switch (action.type){
-        
+
     }
 };
 
