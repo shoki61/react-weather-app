9c482c1c544f0a9a1afd8a068be53a09d726b203
Sohrat <sohrat6128@gmail.com>
2020-11-28 20:24:24 +0300

    convert timestamp in hourlyCard

--- src/components/Forecast/ForecastCards/HourlyCard/HourlyCard.js
+++ src/components/Forecast/ForecastCards/HourlyCard/HourlyCard.js
@@ -12,25 +12,29 @@ const styles = {
     marginRight:3
 };
 
-const hourlyCard = props => (
-    <div className='Hourly-Card'>
-        <div className='Hour'>
-            <p>{props.time}</p>
-        </div>
-        <div className='Hourly-Forecast'>
-            <img src={img} alt='img'/>
-            <div className='An-Hour-Forecast'>
-                <p className='Hour-Temp'>{props.temp}°C</p>
-                <p>{props.weather[0].main}</p>
+
+const hourlyCard = props => {
+    const time = new Date(props.time * 1000).toLocaleTimeString().slice(0, -3)
+    return(
+        <div className='Hourly-Card'>
+            <div className='Hour'>
+                <p>{time}</p>
+            </div>
+            <div className='Hourly-Forecast'>
+                <img src={img} alt='img'/>
+                <div className='An-Hour-Forecast'>
+                    <p className='Hour-Temp'>{props.temp}°C</p>
+                    <p>{props.weather[0].main}</p>
+                </div>
+            </div>
+            <div className='Hourly-Details'>
+                <p><BsFillDropletFill style={{color: '#00a2ff', ...styles}}/> %{props.humidity}</p>
+                <p><IoMdTimer style={{color: '#ff6f00', ...styles}}/> {props.pressure} hpa</p>
+                <p><FiWind style={{...styles}}/> {props.wind} m/s</p>
+                <p><IoIosCloud style={{...styles}}/> %{props.clouds}</p>
             </div>
         </div>
-        <div className='Hourly-Details'>
-            <p><BsFillDropletFill style={{color:'#00a2ff',...styles}}/> %{props.humidity}</p>
-            <p><IoMdTimer style={{color:'#ff6f00',...styles}} /> {props.pressure} hpa</p>
-            <p><FiWind style={{...styles}}/> {props.wind} m/s</p>
-            <p><IoIosCloud style={{...styles}}/> %{props.clouds}</p>
-        </div>
-    </div>
-);
+    )
+};
 
 export default hourlyCard;
