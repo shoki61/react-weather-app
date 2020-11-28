51ddc07374d8518a63f3bb39caee1faa32b3e5a6
Sohrat <sohrat6128@gmail.com>
2020-11-28 20:45:23 +0300

    convert units

--- src/components/CurrentWeather/CurrentWeather.js
+++ src/components/CurrentWeather/CurrentWeather.js
@@ -9,33 +9,37 @@ import Spinner from '../Spinner/Spinner';
 import img from '../../assets/icons/11n.png';
 
 
-const currentWeather = props =>  (
-    <div className='Current-Weather-Card'>
-        {props.currentData
-            ? <div>
-                <p className='City-Name'>{props.locationName}</p>
-                <div className='Weather-Status'>
-                    <img src={img} alt='icon'/>
-                    <p className='Temp'>{props.currentData.temp}</p>
-                </div>
-                <div className='Weather-Details'>
-                    <div className='Details'>
-                        <p>Humidity: %{props.currentData.humidity}</p>
-                        <p>Pressure: {props.currentData.pressure}</p>
+const currentWeather = props => {
+    const temp = Math.round(props.currentData.temp - 273.15);
+    const windSpeed = Math.round(props.currentData.wind_speed * 3.6);
+    return (
+        <div className='Current-Weather-Card'>
+            {props.currentData
+                ? <div>
+                    <p className='City-Name'>{props.locationName}</p>
+                    <div className='Weather-Status'>
+                        <img src={img} alt='icon'/>
+                        <p className='Temp'>{temp}°C</p>
                     </div>
-                    <div className='Details'>
-                        <p>Wind: {props.currentData.wind_speed}</p>
-                        <p>Clouds: %{props.currentData.clouds}</p>
+                    <div className='Weather-Details'>
+                        <div className='Details'>
+                            <p>Humidity: %{props.currentData.humidity}</p>
+                            <p>Pressure: {props.currentData.pressure} hPa</p>
+                        </div>
+                        <div className='Details'>
+                            <p>Wind: {windSpeed} km/h</p>
+                            <p>Clouds: %{props.currentData.clouds}</p>
+                        </div>
                     </div>
+                    <Button btnType='Add'>
+                        <FaRegStar/>
+                        {/*<FaStar/>*/}
+                    </Button>
                 </div>
-                <Button btnType='Add'>
-                    <FaRegStar/>
-                    {/*<FaStar/>*/}
-                </Button>
-            </div>
-            : <Spinner/>
-        }
-    </div>
-);
+                : <Spinner/>
+            }
+        </div>
+    )
+};
 
 export default currentWeather;
--- src/components/Forecast/ForecastCards/HourlyCard/HourlyCard.js
+++ src/components/Forecast/ForecastCards/HourlyCard/HourlyCard.js
@@ -14,7 +14,9 @@ const styles = {
 
 
 const hourlyCard = props => {
-    const time = new Date(props.time * 1000).toLocaleTimeString().slice(0, -3)
+    const time = new Date(props.time * 1000).toLocaleTimeString().slice(0, -3);
+    const temp = Math.round(props.temp - 273.15);
+    const windSpeed = Math.round(props.wind * 3.6);
     return(
         <div className='Hourly-Card'>
             <div className='Hour'>
@@ -23,14 +25,14 @@ const hourlyCard = props => {
             <div className='Hourly-Forecast'>
                 <img src={img} alt='img'/>
                 <div className='An-Hour-Forecast'>
-                    <p className='Hour-Temp'>{props.temp}°C</p>
+                    <p className='Hour-Temp'>{temp}°C</p>
                     <p>{props.weather[0].main}</p>
                 </div>
             </div>
             <div className='Hourly-Details'>
                 <p><BsFillDropletFill style={{color: '#00a2ff', ...styles}}/> %{props.humidity}</p>
-                <p><IoMdTimer style={{color: '#ff6f00', ...styles}}/> {props.pressure} hpa</p>
-                <p><FiWind style={{...styles}}/> {props.wind} m/s</p>
+                <p><IoMdTimer style={{color: '#ff6f00', ...styles}}/> {props.pressure} hPa</p>
+                <p><FiWind style={{...styles}}/> {windSpeed} km/h</p>
                 <p><IoIosCloud style={{...styles}}/> %{props.clouds}</p>
             </div>
         </div>
