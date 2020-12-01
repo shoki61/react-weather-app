a9bbfec2240e80981feecc00b2a43f23aa3d717c
Sohrat <sohrat6128@gmail.com>
2020-12-01 13:44:01 +0300

    chang fav star icon when removing a favorite location if the location is already seen in current weather card

--- src/containers/Weather/Weather.js
+++ src/containers/Weather/Weather.js
@@ -35,6 +35,10 @@ class Weather extends Component{
 
     removeFavoritesHandler = (index) => {
         this.props.onRemoveFavorites(index);
+        setTimeout(()=>{
+            if (this.props.favorites.includes(this.props.location)) this.setState({isFavorite: true});
+            else this.setState({isFavorite: false})
+        },0);
     };
 
     removeAllFavoritesHandler = () => {
