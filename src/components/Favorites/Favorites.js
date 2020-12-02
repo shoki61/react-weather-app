d0674d0411f354a272237017f5ce89988bfad4d7
Sohrat <sohrat6128@gmail.com>
2020-12-02 20:55:18 +0300

    fix an issue that when clicking a favorite location, it is shown in current weather

--- src/components/Favorites/Favorites.js
+++ src/components/Favorites/Favorites.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { IoMdRemoveCircleOutline } from 'react-icons/io';
 
 import './Favorites.css';
@@ -6,18 +6,19 @@ import Button from '../../components/Buttons/Button/Button';
 
 import img from '../../assets/icons/11n.png';
 
-
 const favorites = props => (
-    <div className='Favorites-Card' onClick={props.showFavoriteWeather}>
-        <Button clicked={props.clicked} btnType='Remove'>
-            <IoMdRemoveCircleOutline/>
-        </Button>
-        <p className='City'>{props.locationName}</p>
-        <div style={{display:'flex', alignItems:'center'}}>
-            <img src={img} alt='img'/>
-            <p className='Temperature'>25°C</p>
+        <div className='Favorite-Container'>
+            <Button clicked={props.clicked} btnType='Remove'>
+                <IoMdRemoveCircleOutline/>
+            </Button>
+            <div className='Favorites-Card' onClick={props.showFavoriteWeather}>
+                <p className='City'>{props.locationName}</p>
+                <div style={{display: 'flex', alignItems: 'center'}}>
+                    <img src={img} alt='img'/>
+                    <p className='Temperature'>25°C</p>
+                </div>
+            </div>
         </div>
-    </div>
-);
+)
 
 export default favorites;
