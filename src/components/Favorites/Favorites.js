b1a0140f811667d476deaf339a3568c25f6d0d39
Sohrat <sohrat6128@gmail.com>
2020-12-03 22:03:16 +0300

    fix some responsive

--- src/components/Favorites/Favorites.js
+++ src/components/Favorites/Favorites.js
@@ -7,18 +7,20 @@ import Button from '../../components/Buttons/Button/Button';
 import img from '../../assets/icons/11n.png';
 
 const favorites = props => (
-        <div className='Favorite-Container'>
-            <Button clicked={props.clicked} btnType='Remove'>
-                <IoMdRemoveCircleOutline/>
-            </Button>
-            <div className='Favorites-Card' onClick={props.showFavoriteWeather}>
+    <div className='Favorites-Card'>
+        <div className='City-Info-Container' onClick={props.showFavoriteWeather}>
+            <div>
                 <p className='City'>{props.locationName}</p>
-                <div style={{display: 'flex', alignItems: 'center'}}>
-                    <img src={img} alt='img'/>
-                    <p className='Temperature'>25°C</p>
-                </div>
+            </div>
+            <div style={{display: 'flex', alignItems: 'center'}}>
+                <img src={img} alt='img'/>
+                <p className='Temperature'>25°C</p>
             </div>
         </div>
-)
+        <Button clicked={props.clicked} btnType='Remove'>
+            <IoMdRemoveCircleOutline/>
+        </Button>
+    </div>
+);
 
 export default favorites;
