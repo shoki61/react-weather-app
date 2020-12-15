eadc43d1391611e404764e48a6c26246ab58a72d
Sohrat <sohrat6128@gmail.com>
2020-12-15 11:55:41 +0300

    add github link and switch button in side drawer

--- src/components/SideDrawer/SideDrawer.js
+++ src/components/SideDrawer/SideDrawer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import FavoriteItem from './FavoriteItem/FavoriteItem';
+import SwitchButton from '../Buttons/SwitchButton/SwitchButton';
 import './SideDrawer.css';
 
 const sideDrawer = props => {
@@ -10,7 +11,15 @@ const sideDrawer = props => {
         <div
             className={attachedClasses.join(' ')}
         >
-
+            <div className='Link-Mode-Container'>
+                <a href='https://github.com/shoki61/react-weather-app' rel='noreferrer' target='_blank'>
+                    <i className="fab fa-github"> </i>
+                </a>
+                <div style={{display:'flex',alignItems:'center'}}>
+                    <SwitchButton checked={props.value} clicked={props.clicked}/>
+                </div>
+            </div>
+            <FavoriteItem/>
         </div>
     )
 };
